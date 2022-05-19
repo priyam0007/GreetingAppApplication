b52d9d4e02462d5b0d27f294c1157f4aa77f6955
Your Name <you@example.com>
2022-05-19 13:36:50 +0530

    Added:  used service layer to get simple greeting message

--- src/main/java/com/example/greetingappapplication/controller/GreetingController.java
+++ src/main/java/com/example/greetingappapplication/controller/GreetingController.java
@@ -1,26 +1,34 @@
 package com.example.greetingappapplication.controller;
 
-import com.example.greetingappapplication.model.Greeting;
-import org.springframework.web.bind.annotation.*;
+import com.example.greetingappapplication.service.GreetingService;
 import java.util.concurrent.atomic.AtomicLong;
+import com.example.greetingappapplication.model.Greeting;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 public class GreetingController {
     private static final String template = "Hello %s";
     private static AtomicLong counter = new AtomicLong();
 
+    @Autowired
+    GreetingService greetingService;
     @GetMapping("/getGreeting")
     public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
         return new Greeting(counter.incrementAndGet(), String.format(template, name));
     }
-
     @PostMapping("/postGreeting")
     public Greeting sayHello(@RequestBody Greeting greeting) {
         return new Greeting(counter.incrementAndGet(), String.format(template, greeting.getContent()));
     }
-
     @PutMapping("/putMapping/{counter}")
     public Greeting sayHello(@PathVariable long counter, @RequestParam(value = "content") String content) {
         return new Greeting(counter, String.format(template, content));
+    }
+    @GetMapping("/getMessage")
+    public ResponseEntity<String> getMessage() {
+        return new ResponseEntity<String>(greetingService.getMessage(), HttpStatus.OK);
     }
 }
 
